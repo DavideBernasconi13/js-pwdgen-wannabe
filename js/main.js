0c8fb48176b8563010a04400ea70d2ccbedd6d96
davidebernasconi13 <davideb.bernasconi@gmail.com>
2024-03-05 15:05:03 +0100

    condition no input utente

--- js/main.js
+++ js/main.js
@@ -2,12 +2,23 @@
 // scrivere a schermo nomecognomecolorepreferito23 (nomecognomecolorepreferito23)
 
 let nome = prompt("Come ti chiami");
+
 console.log(nome);
 let cognome = prompt("Quale è il tuo cognome?");
-console.log(cognome);
+//console.log(cognome);
 let colorePreferito = prompt("Indica il tuo colore preferito");
-console.log(colorePreferito);
+//console.log(colorePreferito);
+
+//condizioni se non si inserisce valore
+if (nome == null || nome == "" & cognome == null || cognome == "" & colorePreferito == null || colorePreferito == "") {
+    nome = "valoreNomeNN";
+    cognome = "valoreCognomeNN";
+    colorePreferito = "NessunColore";
+}
+// messaggio da stampare a schermo
+
 let messaggio = nome + cognome + colorePreferito + "23";
 console.log(messaggio);
+
 
 document.getElementById('pswGen').innerHTML = messaggio; 
